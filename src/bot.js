333746b5706554faf166eabba9d3c055577c7279
Mihir Aggarwal <mihir.aggarwal1000@gmail.com>
2022-08-12 03:02:25 +0530

    get roles

--- src/bot.js
+++ src/bot.js
@@ -5,6 +5,7 @@ const {
     ActionRowBuilder,
     ButtonBuilder,
     ButtonStyle,
+    EmbedBuilder,
 } = require("discord.js");
 
 const client = new Client({
@@ -15,13 +16,32 @@ const client = new Client({
     ],
 });
 
+const embedDescription = (roles) => {
+    const start = `Here are the roles present in the server. You may choose all the ones you wish to assign tasks to\
+    \n\n\
+    **Roles**\n\n\
+    \
+    `
+    let end = ''
+    roles.forEach(role => {end += `${role}\n`})
+    let final = start + end
+    return final
+}
+
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', (message)=>{
+    const roles = message.guild.roles.cache.map(role => role.name)
+    const desc = embedDescription(roles)
     if(message.mentions.has(client.user.id)){
-        
+        message.reply({ embeds: [
+            new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle('Rollertoaster')
+            .setDescription(desc)
+        ] })
     }
 })
 
