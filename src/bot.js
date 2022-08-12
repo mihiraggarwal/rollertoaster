e9c924ea94e679aef42d3dd0b4d112cfc16cc4e5
Mihir Aggarwal <mihir.aggarwal1000@gmail.com>
2022-08-12 13:12:50 +0530

    save roles

--- src/bot.js
+++ src/bot.js
@@ -17,6 +17,9 @@ const client = new Client({
 });
 
 const emojis = ['🔴','🟠','🟡','🟢','🔵','🟣','🟤','⚫','⚪','🟥','🟧','🟨','🟩','🟦','🟪','🟫','⬛','⬜','🔶','🔷']
+let assign = {}
+let selection = []
+let reactmsg = ''
 
 const embedDescription = (roles) => {
     const start = `Here are the roles present in the server. You may choose all the ones you wish to assign tasks to\
@@ -27,10 +30,19 @@ const embedDescription = (roles) => {
     let end = ''
     for (i = 0; i < roles.length; i++) {
         end += `${emojis[i]} : ${roles[i]}\n`
+        assign[emojis[i]] = roles[i]
     }
     let final = start + end
     return final
 }
+
+const finReactBtn = new ActionRowBuilder()
+    .addComponents(
+        new ButtonBuilder()
+        .setCustomId('taskOn')
+        .setLabel('Add Tasks')
+        .setStyle(ButtonStyle.Primary)
+    )
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -47,7 +59,32 @@ client.on('messageCreate', (message)=>{
             .setDescription(desc)
         ] }).then(eMessage => {
                 emojis.slice(0, roles.length).forEach(emoji => eMessage.react(emoji))
+                reactmsg = eMessage
         })
+        message.channel.send({
+            content: "Once you have selected the roles, you may begin adding the tasks.",
+            components: [finReactBtn]
+        })
+    }
+})
+
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isButton()) return;
+
+    if (interaction.customId === "taskOn") {
+        const msg = await reactmsg.fetch()
+        const reacts = await msg.reactions.cache
+        reacts.forEach(emoji => {
+            if (emoji.count > 1) {
+                selection.push(assign[emoji._emoji.name])
+            }
+        })
+        msg.edit({ embeds: [
+            new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle('Rollertoaster')
+            .setDescription('Roles saved')
+        ] }).then(msg.reactions.removeAll())
     }
 })
 
